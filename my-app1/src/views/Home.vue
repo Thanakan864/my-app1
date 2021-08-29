84eda6b84180204089fde3e6b6a78bba4f0d5e8a
Thanakan864 <thanakan864@gmail.com>
2021-08-29 15:35:45 +0700

    clean code
    delete code init of vuetify

--- my-app1/src/views/Home.vue
+++ my-app1/src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
-  <hello-world />
+  <p>Home page</p>
+  <!-- <hello-world /> -->
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import HelloWorld from "../components/HelloWorld";
 
-  export default {
-    name: 'Home',
+export default {
+  name: "Home",
 
-    components: {
-      HelloWorld,
-    },
-  }
+  components: {
+    HelloWorld,
+  },
+};
 </script>
